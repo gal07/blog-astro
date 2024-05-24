0d223e14092b1c79e40287e35daeeace92d7e166
Galih kurnia <galihkur29@gmail.com>
2024-05-25 00:57:49 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://master--blog-astro4455.netlify.app/",
+  integrations: [preact()]
+});
